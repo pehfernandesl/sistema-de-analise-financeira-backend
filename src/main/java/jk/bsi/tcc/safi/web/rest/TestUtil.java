a767b2a1b3afbcb8db2d503f49ba359cddad7766
Pedro Henrique Fernandes <pehfernandesl@gmail.com>
2020-06-07 18:24:58 -0300

    feat: adiciona método para verificar os métodos equals/hashcode do objeto de domínio

--- src/main/java/jk/bsi/tcc/safi/web/rest/TestUtil.java
+++ src/main/java/jk/bsi/tcc/safi/web/rest/TestUtil.java
@@ -1,9 +1,13 @@
 package jk.bsi.tcc.safi.web.rest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Utility class for testing REST controllers.
@@ -27,9 +31,30 @@ public final class TestUtil {
    *
    * @param object the object to convert.
    * @return the JSON byte array.
-   * @throws IOException
+   * @throws IOException the exception.
    */
   public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
     return mapper.writeValueAsBytes(object);
+  }
+
+  /**
+   * Verifies the equals/hashcode contract on the domain object.
+   */
+  public static <T> void equalsVerifier(Class<T> clazz)
+    throws NoSuchMethodException, IllegalAccessException, InvocationTargetException,
+    InstantiationException {
+    T domainObject1 = clazz.getConstructor().newInstance();
+    assertThat(domainObject1.toString()).isNotNull();
+    assertThat(domainObject1).isEqualTo(domainObject1);
+    assertThat(domainObject1.hashCode()).isEqualTo(domainObject1.hashCode());
+    // Test with an instance of another class
+    Object testOtherObject = new Object();
+    assertThat(domainObject1).isNotEqualTo(testOtherObject);
+    assertThat(domainObject1).isNotEqualTo(null);
+    // Test with an instance of the same class
+    T domainObject2 = clazz.getConstructor().newInstance();
+    assertThat(domainObject1).isNotEqualTo(domainObject2);
+    // HashCodes are equals because the objects are not persisted yet
+    assertThat(domainObject1.hashCode()).isEqualTo(domainObject2.hashCode());
   }
 }
