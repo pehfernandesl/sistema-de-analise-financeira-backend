4a1208951c22d1a7239661f324f4001fd8c15a70
Pedro Henrique Fernandes <pehfernandesl@gmail.com>
2020-06-07 15:26:43 -0300

    chore: cria app usando o gerador https://start.spring.io/

---
+++ src/main/java/jk/bsi/tcc/safi/Sistema.java
@@ -0,0 +1,11 @@
+package jk.bsi.tcc.safi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sistema {
+  public static void main(String[] args) {
+    SpringApplication.run(Sistema.class, args);
+  }
+}
