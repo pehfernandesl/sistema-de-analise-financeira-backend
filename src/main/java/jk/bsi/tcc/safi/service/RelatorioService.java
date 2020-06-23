c22d95dc5736c5a299e31bac8968b7a82da8cae7
Pedro Henrique <pehfernandesl@gmail.com>
2020-06-22 21:03:51 -0300

    Adiciona queries de análise

--- src/main/java/jk/bsi/tcc/safi/service/RelatorioService.java
+++ src/main/java/jk/bsi/tcc/safi/service/RelatorioService.java
@@ -1,11 +1,14 @@
 package jk.bsi.tcc.safi.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import jk.bsi.tcc.safi.service.dto.RelatorioDto;
+import jk.bsi.tcc.safi.service.dto.RelatorioOperacoesDto;
+import jk.bsi.tcc.safi.service.util.CoreUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.buf.StringUtils;
@@ -22,8 +25,12 @@ public class RelatorioService {
   private final ActiveUserService activeUserService;
 
   public RelatorioDto preencherRelatorio(Long mes) throws RuntimeException {
+    final List<Long> ids = Arrays.asList(1L, 213L);
+
     final RelatorioDto relatorioDto =
-      this.relatorioRepository.buscarDadosGerais(Arrays.asList(1L, 213L));
+      this.relatorioRepository.buscarDadosGerais(ids);
+    relatorioDto.setOperacoes(relatorioRepository.buscarDadosOperacoes(ids));
+
     return relatorioDto;
   }
 
@@ -41,28 +48,75 @@ class RelatorioRepository {
       "(select res.data as data_maior_mov_pos from (select distinct data , count(*) from transacao where extrato_id in ({}) and cast(valor as text) like '%-%' group by data order by count(*) DESC LIMIT 1) as res),\n" +
       "(select res.data as data_maior_mov_neg from (select distinct data , count(*) from transacao where extrato_id in ({}) and cast(valor as text) not like '%-%' group by data order by count(*) DESC LIMIT 1) as res)";
 
+  private static final String QUERY_DADOS_OPERACOES =
+    "select tipo, cast(sum(valor)as DECIMAL(7,2)),cast(sum(valor) * 100 /\n" +
+      "(select sum(valor)from transacao where extrato_id in ({}) and cast(valor as text)  like '%-%')\n" +
+      "as DECIMAL(7,2))\n" +
+      "from transacao where extrato_id in ({}) and cast(valor as text)  like '%-%' group by tipo order by tipo";
+
   private final EntityManager entityManager;
 
   public RelatorioDto buscarDadosGerais(List<Long> ids) {
     RelatorioDto relatorio = new RelatorioDto();
     final Query query = retornaQueryDadosGerais(ids);
-    relatorio = (RelatorioDto) query.getSingleResult();
+    Object[] obj = (Object[]) query.getSingleResult();
+
+    if (obj[0] != null) {
+      relatorio.setTotal(new Double(obj[0].toString()));
+    }
+    if (obj[1] != null) {
+      relatorio.setTotalEntrada(new Double(obj[1].toString()));
+    }
+    if (obj[2] != null) {
+      relatorio.setTotalSaida(new Double(obj[2].toString()));
+    }
+    if (obj[3] != null) {
+      relatorio.setDataMaisMovimento(CoreUtil.retornarDataFormatada(obj[3].toString()));
+    }
+    if (obj[4] != null) {
+      relatorio.setDataMaisMovimentoPositivo(CoreUtil.retornarDataFormatada(obj[4].toString()));
+    }
+    if (obj[5] != null) {
+      relatorio.setDataMaisMovimentoNegativo(CoreUtil.retornarDataFormatada(obj[5].toString()));
+    }
 
     return relatorio;
   }
 
-  private List<Object> buscarDadosOperacoes(List<Long> ids) {
-    return null;
+  public List<RelatorioOperacoesDto> buscarDadosOperacoes(List<Long> ids) {
+    final List<RelatorioOperacoesDto> lista =
+      new ArrayList<RelatorioOperacoesDto>();
+    final Query query = retornaQueryDadosOperacoes(ids);
+    final List<Object[]> result = query.getResultList();
+
+    for (Object[] obj : result) {
+      RelatorioOperacoesDto op = new RelatorioOperacoesDto();
+      if (obj[0] != null) {
+        op.setTpOperacao(Long.valueOf(obj[0].toString()));
+      }
+      if (obj[1] != null) {
+        op.setValor(Double.valueOf(obj[1].toString()));
+      }
+      if (obj[2] != null) {
+        op.setPercent(Double.valueOf(obj[2].toString()));
+      }
+      lista.add(op);
+    }
+
+    return lista;
   }
 
   private Query retornaQueryDadosGerais(List<Long> ids) {
     final String listaIds =
       StringUtils.join(ids.stream().map(id -> id.toString()).collect(Collectors.toList()), ',');
     return entityManager
-      .createNativeQuery(QUERY_RELATORIO_GERAL.replace("{}", listaIds), RelatorioDto.class);
+      .createNativeQuery(QUERY_RELATORIO_GERAL.replace("{}", listaIds));
   }
 
-  private List<Object> retornaQueryDadosOperacoes(List<Long> ids) {
-    return null;
+  private Query retornaQueryDadosOperacoes(List<Long> ids) {
+    final String listaIds =
+      StringUtils.join(ids.stream().map(id -> id.toString()).collect(Collectors.toList()), ',');
+    return entityManager
+      .createNativeQuery(QUERY_DADOS_OPERACOES.replace("{}", listaIds));
   }
 }
