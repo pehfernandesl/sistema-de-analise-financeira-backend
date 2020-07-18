802000d5cd07f34c46c55df23157bbf761047a62
Pedro Henrique Fernandes <pehfernandesl@gmail.com>
2020-07-18 19:22:01 -0300

    feat: adiciona verificação para perfis ativos + logging de informações básicas ao iniciar a aplicação

--- src/main/java/jk/bsi/tcc/safi/Sistema.java
+++ src/main/java/jk/bsi/tcc/safi/Sistema.java
@@ -1,35 +1,75 @@
 package jk.bsi.tcc.safi;
 
-import java.time.LocalDate;
-import java.time.Month;
-import jk.bsi.tcc.safi.domain.Usuario;
-import jk.bsi.tcc.safi.repository.UsuarioRepository;
-import org.springframework.boot.CommandLineRunner;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import javax.annotation.PostConstruct;
+import jk.bsi.tcc.safi.config.ActiveProfilesConstants;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 
+@Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication
 public class Sistema {
+  private final Environment env;
+
   public static void main(String[] args) {
-    SpringApplication.run(Sistema.class, args);
+    SpringApplication sistema = new SpringApplication(Sistema.class);
+    ConfigurableEnvironment env = sistema.run(args).getEnvironment();
+    logApplicationStartup(env);
   }
 
-  @Profile({"test", "dev"})
-  @Bean
-  CommandLineRunner commandLineRunner(UsuarioRepository usuarioRepository,
-                                      PasswordEncoder passwordEncoder) {
-    return args -> {
-      Usuario usuario = new Usuario();
+  /**
+   * Spring profiles can be configured with a program argument --spring.profiles.active=your-active-profile
+   */
+  @PostConstruct
+  public void initApplication() {
+    boolean containsDevProfile = Arrays.asList(env.getActiveProfiles()).contains(
+      ActiveProfilesConstants.SPRING_DEVELOPMENT_PROFILE);
+    boolean containsProdProfile = Arrays.asList(env.getActiveProfiles())
+      .contains(ActiveProfilesConstants.SPRING_PRODUCTION_PROFILE);
 
-      usuario.setEmail("admin@safi.net");
-      usuario.setSenha(passwordEncoder.encode("admin"));
-      usuario.setDataNascimento(LocalDate.of(1998, Month.JUNE, 29));
-      usuario.setNome("Administrador");
+    if (containsDevProfile && containsProdProfile) {
+      log.error(
+        "You have misconfigured your application! It should not run with both the 'dev' and 'prod' profiles at the same time.");
+    }
+  }
 
-      usuarioRepository.save(usuario);
-    };
+  private static void logApplicationStartup(Environment env) {
+    String protocol = "http";
+    if (env.getProperty("server.ssl.key-store") != null) {
+      protocol = "https";
+    }
+    String serverPort = env.getProperty("server.port");
+    String contextPath = env.getProperty("server.servlet.context-path");
+    if (StringUtils.isEmpty(contextPath)) {
+      contextPath = "/";
+    }
+    String hostAddress = "localhost";
+    try {
+      hostAddress = InetAddress.getLocalHost().getHostAddress();
+    } catch (UnknownHostException e) {
+      log.warn("The host name could not be determined, using `localhost` as fallback");
+    }
+    log.info("\n----------------------------------------------------------\n\t" +
+        "Application '{}' is running! Access URLs:\n\t" +
+        "Local: \t\t{}://localhost:{}{}\n\t" +
+        "External: \t{}://{}:{}{}\n\t" +
+        "Profile(s): \t{}\n----------------------------------------------------------",
+      env.getProperty("spring.application.name"),
+      protocol,
+      serverPort,
+      contextPath,
+      protocol,
+      hostAddress,
+      serverPort,
+      contextPath,
+      env.getActiveProfiles());
   }
 }
---
+++ src/main/java/jk/bsi/tcc/safi/config/ActiveProfilesConstants.java
@@ -0,0 +1,6 @@
+package jk.bsi.tcc.safi.config;
+
+public final class ActiveProfilesConstants {
+  public static final String SPRING_DEVELOPMENT_PROFILE = "dev";
+  public static final String SPRING_PRODUCTION_PROFILE = "prod";
+}
